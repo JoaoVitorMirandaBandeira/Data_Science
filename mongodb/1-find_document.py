fe12c9d935bc81e4f8148d74116201a596533035
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2024-03-12 23:56:25 -0300

    Aprendendo mais sobre estrutura de pesquisa em documentos usando o mongoDB

---
+++ mongodb/1-find_document.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient
+
+# 1 - Conectando com o banco MongoDB e o Database
+client = MongoClient()
+db = client['nobel']
+print(db.list_collection_names())
+prizes = db['prizes']
+laureates = db['laureates']
+
+# 2 - Contando documentos pelo genero
+print(db.laureates.count_documents({'gender': 'female'}))
+print(db.laureates.count_documents({'gender': 'male'}))
+
+# 3 - Filtro composto com mais informações
+
+filter_doc= {
+    'diedCountry': 'France',
+    'gender': 'female',
+    'bornCity': 'Warsaw'
+}
+
+print(db.laureates.count_documents(filter_doc))
+
+# 4 - Buscando documento pelo filtro
+
+print(db.laureates.find_one(filter_doc))
+
+# 5 - Usando os operadore in(sql) e ne(not)
+
+print(db.laureates.count_documents({'diedCountry': {'$in': ['France', 'Germany']}}))
+print(db.laureates.count_documents({'diedCountry': {'$ne': 'France'}}))
+
+# 6 - Buscando em subestruturas
+
+california = db.laureates.count_documents({
+    'prizes.affiliations.name': 'University of California'
+})
+
+print(california)
+
+san_francisco = db.laureates.count_documents({
+    'prizes.affiliations.city': 'San Francisco, CA'
+})
+
+print(san_francisco)
